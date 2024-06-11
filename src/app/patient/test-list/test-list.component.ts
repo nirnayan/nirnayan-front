d8dd820b61fb702026ebee9fbfd4b5a6d50bcf60
Ali Rehan Haider <007rehan16@gmail.com>
2024-06-11 17:27:09 +0530

    Changes-rehan

--- src/app/patient/test-list/test-list.component.ts
+++ src/app/patient/test-list/test-list.component.ts
@@ -43,7 +43,7 @@ export class TestListComponent implements OnInit {
       1450: {
         items: 12
       },
-    }, dots: false, nav: true, loop: true
+    }, dots: false, nav: true,
   };
 
   basePath: any = environment.BaseLimsApiUrl
@@ -63,14 +63,14 @@ export class TestListComponent implements OnInit {
   groupId: any
 
 
-  constructor(private _master: MasterService, private _spiner: NgxSpinnerService,
+  constructor(private _master: MasterService, private spiner: NgxSpinnerService,
     private _route: Router,
-    private _auth: AuthService, private _cart: CartService,
+    private auth: AuthService, private _cart: CartService,
     private _router: Router) { }
 
   ngOnInit(): void {
 
-    this.isLogin = this._auth.isLoggedIn()
+    this.isLogin = this.auth.isLoggedIn()
     $("#loader").show();
     this.getAllGroups();
     AOS.init();
@@ -162,11 +162,7 @@ export class TestListComponent implements OnInit {
     });
   }
 
-<<<<<<< HEAD
-  filterTests(group_id: any, group_name: any) {
-=======
-  filterTests(group_id:any, group_name:any , activeGroup:any){
->>>>>>> 9b502c1315757c53c2a4d68aa27b8c924cea07c3
+  filterTests(group_id: any, group_name: any, activeGroup: any) {
     // $("#loader").show();
     this.activeGroupName = group_name;
     // const formData = new FormData();
@@ -183,45 +179,38 @@ export class TestListComponent implements OnInit {
     //   console.log(err);
     //   $("#loader").hide();
     // });
-<<<<<<< HEAD
-    this.groupId = group_id
-    const state = 36;
-    const limit = 18;
-    const lastId = 0;
-=======
-    if(activeGroup == 'Organ'){
-    this.groupId =group_id
-    const state = 36; 
-    const limit = 18; 
-    const lastId = 0; 
->>>>>>> 9b502c1315757c53c2a4d68aa27b8c924cea07c3
-    const groupId = group_id
-    this._master.getAllNewTests(state, limit, lastId, groupId).subscribe((res: any) => {
-      if (res.status == 1) {
-        this.isLoading = false;
-        this.testItems = res.data
-        // this.lastItemId = this.testItems[this.testItems.length - 1].id
-        // this.testItems = localArr.concat(res.data)
-        // this.lastItemId = this.testItems[this.testItems.length - 1].id
-      }
-    })
-  }else if(activeGroup == 'Condition'){
-    this.groupId =group_id
-    const state = 36; 
-    const limit = 18; 
-    const lastId = 0; 
-    const groupId = group_id
-    this._master.getAllNewPackages(state,limit,lastId,groupId).subscribe((res:any) => {
-      if(res.status==1) {
-        this.isLoading = false;
-        this.testItems = res.data
-        // this.lastItemId = this.testItems[this.testItems.length - 1].id
-        // this.testItems = localArr.concat(res.data)
-        // this.lastItemId = this.testItems[this.testItems.length - 1].id
-      }
-  })
+    if (activeGroup == 'Organ') {
+      this.groupId = group_id
+      const state = 36;
+      const limit = 18;
+      const lastId = 0;
+      const groupId = group_id
+      this._master.getAllNewTests(state, limit, lastId, groupId).subscribe((res: any) => {
+        if (res.status == 1) {
+          this.isLoading = false;
+          this.testItems = res.data
+          // this.lastItemId = this.testItems[this.testItems.length - 1].id
+          // this.testItems = localArr.concat(res.data)
+          // this.lastItemId = this.testItems[this.testItems.length - 1].id
+        }
+      })
+    } else if (activeGroup == 'Condition') {
+      this.groupId = group_id
+      const state = 36;
+      const limit = 18;
+      const lastId = 0;
+      const groupId = group_id
+      this._master.getAllNewPackages(state, limit, lastId, groupId).subscribe((res: any) => {
+        if (res.status == 1) {
+          this.isLoading = false;
+          this.testItems = res.data
+          // this.lastItemId = this.testItems[this.testItems.length - 1].id
+          // this.testItems = localArr.concat(res.data)
+          // this.lastItemId = this.testItems[this.testItems.length - 1].id
+        }
+      })
+    }
   }
-}
 
   // Get All Groups
   getAllGroups() {
