b99b9ac74c67dab6e5fbedc2d5731c5d7d03259f
Arijit <arijitghosal1000@gmail.com>
2024-06-27 16:29:05 +0530

    changes

---
+++ src/app/service/ng2-search.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { Ng2SearchPipe } from './ng2-search.pipe';
+
+describe('Ng2SearchPipe', () => {
+  it('create an instance', () => {
+    const pipe = new Ng2SearchPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
