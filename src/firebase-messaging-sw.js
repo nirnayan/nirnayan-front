861c58c0f2d3aaacd123a75cc2c2538f7878dcb8
Arijit <arijitghosal1000@gmail.com>
2024-07-04 13:45:02 +0530

    "Updated profile component, association component, header component, and notification component with changes to HTML, CSS, and TS files; also updated environment files and firebase-messaging-sw.js"

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,12 +1,12 @@
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 firebase.initializeApp({
- apiKey: "AIzaSyD_5WxJm0SWFsz0YeWZLiGaetmqjHfPpw4",
- authDomain: "b2c-front.firebaseapp.com",
- projectId: "b2c-front",
- storageBucket: "b2c-front.appspot.com",
- messagingSenderId: "955156638217",
- appId: "1:955156638217:web:10610b3e3daa6f0b2426d8",
- measurementId: "G-5M2XQYLVD8",
+    apiKey: "AIzaSyD_5WxJm0SWFsz0YeWZLiGaetmqjHfPpw4",
+    authDomain: "b2c-front.firebaseapp.com",
+    projectId: "b2c-front",
+    storageBucket: "b2c-front.appspot.com",
+    messagingSenderId: "955156638217",
+    appId: "1:955156638217:web:06981e0cdd8b25002426d8",
+    measurementId: "G-DR8W97JHQY"
 });
 const messaging = firebase.messaging();
